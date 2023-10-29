203fc585bef86da181e593814f6fea34090495a2
Tram Anh Nguyen <anhng148@gmail.com>
2023-10-29 15:10:30 -0500

    close #15

--- js/login.js
+++ js/login.js
@@ -1,32 +1,46 @@
+// var front = "https://anhnguyen148.github.io/savory-journey-website/";
+// var back = "https://qanguyen.net/recipes-backend/";
+var front = "http://localhost:5500/";
+var back = "http://localhost/recipes-backend/";
+
 $(document).ready(function () {
+
   $(".login-form").on("submit", function (e) {
     e.preventDefault();
+
     let username = $("#username").val();
     let password = $("#password").val();
-    let user = {
-      username: username,
-      password: password
-    };
-    fetch("http://localhost:3001/login", {
+
+    if (!username) {
+      $('.message').text('Please enter your username.');
+      return;
+    } else if (!password) {
+      $('.message').text('Please enter your password.');
+      return;
+    }
+
+    let user = new FormData();
+    user.append('username', username);
+    user.append('password', password);
+
+    fetch(back + "login.php", {
       method: "POST",
-      headers: {
-        "Content-Type": "application/json"
-      },
-      body: JSON.stringify(user)
+      body: user
     })
     .then(response => response.json())
     .then(json => {
-      console.log(json);
-      if (json.message === "success") {
+      // console.log(json);
+      if (json.status === "OK") {
         localStorage.setItem("user", JSON.stringify(json.data));
-        window.location.href = "https://anhnguyen148.github.io/savory-journey-website/profile.html";
-      } else {
-        alert("Invalid email or password");
+        localStorage.setItem('token', JSON.stringify(json.data.token));
+        window.location.href = front + "profile.html";
+      } else { // status === "NOT OK"
+        $('.message').text(json.message);
       }
     });
   });
 });
 
 function move(page) {
-  window.location.href = "https://anhnguyen148.github.io/savory-journey-website/" + page + ".html";
+  window.location.href = front + page + ".html";
 }
--- js/signup.js
+++ js/signup.js
@@ -1,6 +1,11 @@
+// var front = "https://anhnguyen148.github.io/savory-journey-website/";
+// var back = "https://qanguyen.net/recipes-backend/";
+var front = "http://localhost:5500/";
+var back = "http://localhost/recipes-backend/";
+
 $(document).ready(function () {
 });
 
 function move(page) {
-  window.location.href = "https://anhnguyen148.github.io/savory-journey-website/" + page + ".html";
+  window.location.href = front + page + ".html";
 }
